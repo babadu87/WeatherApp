de12e02b8df66d6321e7685bcfdbda67d75b604c
LSandrine <latour.sandrine.he@gmail.com>
2017-03-28 16:40:44 +0200

    ajout de auto complete

---
+++ app/src/main/java/com/example/boismorand/weatherapp/Autocompl.java
@@ -0,0 +1,31 @@
+package com.example.boismorand.weatherapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Latour on 28/03/2017.
+ */
+
+public class Autocompl extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.autocomplete);
+        //final ArrayList<String> ListesVilles = getIntent().getParcelableExtra("Listevilles");
+
+        Log.i("Autocompl","coucou");
+/*
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, ListesVilles);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.autocomplete);
+        textView.setAdapter(adapter);*/
+
+    }
+}
